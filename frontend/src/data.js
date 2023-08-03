cce2ea27acac0903704a88862531f66d48004a49
Rob <robbdasailer@icloud.com>
2023-08-03 09:56:07 +0200

    video 09

---
+++ frontend/src/data.js
@@ -0,0 +1,30 @@
+const data = {
+    products: [
+        {
+            name: 'Nike Slim Shirt',
+            slug: 'nike-slim-shirt',
+            category: 'Shirts',
+            image:'images/p1.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            name: 'Nike Shorts',
+            slug: 'nike-shorts',
+            category: 'Shirts',
+            image:'images/p2.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+    ]
+}
+
+export default data;
